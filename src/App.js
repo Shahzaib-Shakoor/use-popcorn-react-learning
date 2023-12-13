7bf95aaa710bbadf7201fbcf806b092241ad4ecd
Shahzaib Shakoor <shahzaib.shakoor2199@gmail.com>
2023-12-13 12:06:37 +0500

    made it better

--- src/App.js
+++ src/App.js
@@ -246,7 +246,7 @@ function MovieDetails({
       async function getMovieDetails() {
         setISLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
